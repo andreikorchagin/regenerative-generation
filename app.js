a9c32d52fe2a005559628fa153773d0b9a68ad45
andreikorchagin <andrei.korchagin@gmail.com>
2021-09-08 21:03:32 -0400

    Minor refactor, added path to image save location

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 const p5 = require('node-p5');
+const side = 400;
+const num_slices = 3;
+const slice = side / num_slices;
+const circle_size = 100;
+const random_limit = 100000;
+const dir = 'images/';
 
 function sketch(p) {
     p.setup = () => {
-        let side = 400;
         let canvas = p.createCanvas(side, side);
-        let canvas_name = p.random(100000).toFixed().toString();
+        let canvas_name = p.random(random_limit).toFixed().toString();
         p.colorMode(p.HSB);
         p.background(p.random(255), p.random(255), p.random(255));
         p.strokeWeight(0);
         p.noLoop();
         setTimeout(() => {
-            p.saveCanvas(canvas, canvas_name, 'png').then(filename => {
+            p.saveCanvas(canvas, dir.concat(canvas_name), 'png').then(filename => {
                 console.log(`saved the canvas as ${filename}`);
             });
         }, 100);
     }
     p.draw = () => {
-        let num_slices = 3;
-        let side = 400;
-        let slice = side / num_slices;
-        let circle_size = 100;
         for (var i = 0; i < num_slices + 1; i++) {
             for (var j = 0; j < num_slices + 1; j++) {
                 p.fill(p.random(255), p.random(255), p.random(255));
