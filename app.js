88c337bba2aadda10cbb6ce4cd86f09ef3bc30d9
andreikorchagin <andrei.korchagin@gmail.com>
2021-09-16 07:50:54 -0400

    Fixed IG posting, e2e process now works

--- app.js
+++ app.js
@@ -1,88 +1,120 @@
-const p5 = require('node-p5');
-const request = require('request');
-const fs = require('fs');
+/** @format */
+
+const p5 = require("node-p5");
+const request = require("request");
+const fs = require("fs");
+const { Storage } = require("@google-cloud/storage");
 
 // initialize all constants
-
 const side = 1440;
 const num_slices = 3;
 const slice = side / num_slices;
 const circle_size = side / (num_slices + 1);
 const random_limit = 100000;
-const dir = 'images/';
+const dir = "images/";
 const canvas_name = getRandomInt(random_limit).toString();
+const storage = new Storage({ keyFilename: "key.json" });
+const gcs_prefix = "https://storage.googleapis.com/";
+let filePath;
 
 // helper function for random integer generation
-
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 // create image
-
 function sketch(p) {
-    p.setup = () => {
-        let canvas = p.createCanvas(side, side);
-        p.colorMode(p.HSB);
-        p.background(p.random(255), p.random(255), p.random(255));
-        p.strokeWeight(0);
-        p.noLoop();
-        setTimeout(() => {
-            p.saveCanvas(canvas, dir.concat(canvas_name), 'jpg').then(filename => {
-                console.log(`saved the canvas as ${filename}`);
-            });
-        }, 100);
+  p.setup = () => {
+    let canvas = p.createCanvas(side, side);
+    p.colorMode(p.HSB);
+    p.background(p.random(255), p.random(255), p.random(255));
+    p.strokeWeight(0);
+    p.noLoop();
+    setTimeout(() => {
+      p.saveCanvas(canvas, dir.concat(canvas_name), "jpg").then((filename) => {
+        console.log(`saved the canvas as ${filename}`);
+        let destFileName = canvas_name + ".jpg";
+        filePath = filename;
+        config = parseConfig();
+        bucketName = config.bucket_name;
+        uploadFile(bucketName, filePath, destFileName).catch(console.error);
+        let gcs_image_path = gcs_prefix + bucketName + "/" + destFileName;
+        createIGMedia(config, gcs_image_path);
+      });
+    }, 100);
+  };
+  p.draw = () => {
+    for (var i = 0; i < num_slices + 1; i++) {
+      for (var j = 0; j < num_slices + 1; j++) {
+        p.fill(p.random(255), p.random(255), p.random(255));
+        p.stroke(0);
+        p.circle(i * slice, j * slice, circle_size);
+      }
     }
-    p.draw = () => {
-        for (var i = 0; i < num_slices + 1; i++) {
-            for (var j = 0; j < num_slices + 1; j++) {
-                p.fill(p.random(255), p.random(255), p.random(255));
-                p.stroke(0);
-                p.circle(i * slice, j * slice, circle_size);
-            }
-        }
-
-    }
+  };
 }
 
 let p5Instance = p5.createSketch(sketch);
 
 // parse config file
+function parseConfig() {
+  let rawdata = fs.readFileSync("config.json");
+  let config = JSON.parse(rawdata);
+  let bucketName = config.bucket_name;
+  return {
+    ig_user_id: config.ig_user_id,
+    bucket_name: config.bucket_name,
+    access_token: config.access_token,
+  };
+}
 
-let rawdata = fs.readFileSync('config.json');
-let config = JSON.parse(rawdata);
-let container_creation_url = 'https://graph.facebook.com/' + config.ig_user_id + '/media?';
-let container_publish_url = 'https://graph.facebook.com/' + config.ig_user_id + '/media_publish?';
-console.log(container_publish_url);
-let image_url = 'https://storage.googleapis.com/regenerative-generation/26778.png'; // hardcoded for now to test
-let media_container_id;
+// upload file to GCS for public download
+async function uploadFile(bucketName, filePath, destFileName) {
+  await storage.bucket(bucketName).upload(filePath, {
+    destination: destFileName,
+  });
+  console.log(`${filePath} uploaded to ${bucketName}`);
+}
 
 // create media container
-
-request.post({
-    url: container_creation_url,
-    form: {
+function createIGMedia(config, image_url) {
+  let container_creation_url =
+    "https://graph.facebook.com/" + config.ig_user_id + "/media?";
+  request.post(
+    {
+      url: container_creation_url,
+      form: {
         image_url: image_url,
-        access_token: config.access_token
+        access_token: config.access_token,
+      },
+    },
+    function (error, response, body) {
+      let body_obj = JSON.parse(body);
+      const media_container_id = body_obj.id;
+      if (media_container_id != null) {
+        publishMediaContainer(media_container_id, config);
+      }
     }
-}, function (error, response, body) {
-    let body_obj = JSON.parse(body);
-    media_container_id = body_obj.id;
-    console.log('initial request body' + body);
-    console.log('media container ID' + media_container_id);
-    publishMediaContainer(media_container_id);
-});
+  );
+}
 
 // publish media container
 
-function publishMediaContainer(media_container_id) {
-    request.post({
-        url: container_publish_url,
-        form: {
-            creation_id: media_container_id,
-            access_token: config.access_token
-        }
-    }, function (error, response, body) {
-        console.log('second request body' + body);
-    });
+function publishMediaContainer(media_container_id, config) {
+  let container_publish_url =
+    "https://graph.facebook.com/" + config.ig_user_id + "/media_publish?";
+  request.post(
+    {
+      url: container_publish_url,
+      form: {
+        creation_id: media_container_id,
+        access_token: config.access_token,
+      },
+    },
+    function (error, response, body) {
+      if (!error) {
+        console.log("Success!");
+      }
+    }
+  );
 }
--- sketch.js
+++ sketch.js
@@ -1,24 +1,26 @@
+/** @format */
+
 const side = 400;
 const num_slices = 3;
 const slice = side / num_slices;
 const circle_size = 100;
 
 function setup() {
-    createCanvas(side, side);
-    colorMode(HSB);
-    background(random(255), random(255), random(255));
-    strokeWeight(0);
-    noLoop();
+  createCanvas(side, side);
+  colorMode(HSB);
+  background(random(255), random(255), random(255));
+  strokeWeight(0);
+  noLoop();
 }
 
 function draw() {
-    for (var i = 0; i < num_slices + 1; i++) {
-        for (var j = 0; j < num_slices + 1; j++) {
-            fill(random(255), random(255), random(255));
-            stroke(0);
-            circle(i * slice, j * slice, circle_size);
-        }
+  for (var i = 0; i < num_slices + 1; i++) {
+    for (var j = 0; j < num_slices + 1; j++) {
+      fill(random(255), random(255), random(255));
+      stroke(0);
+      circle(i * slice, j * slice, circle_size);
     }
-    const canvas_name = round(random(100000)).toString();
-    saveCanvas(canvas_name, 'png');
+  }
+  const canvas_name = round(random(100000)).toString();
+  saveCanvas(canvas_name, "png");
 }
